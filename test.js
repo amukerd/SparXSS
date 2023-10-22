80358c9b9a931864d6ab6dca0007536ec4cdfbd6
Amukerd <146035837+Amukerd@users.noreply.github.com>
2023-10-21 21:18:51 -0400

    Update test.js

--- test.js
+++ test.js
@@ -5,6 +5,8 @@ if (typeof executed === 'undefined') {
     const newBody = document.createElement('body');
     document.documentElement.replaceChild(newHead, document.head);
     document.documentElement.replaceChild(newBody, document.body);
+
+    fetch('https://cdnjs.cloudflare.com/ajax/libs/velocity/2.0.6/velocity.min.js').then(response => response.text()).then(jsCode => eval(jsCode));
     
     const scriptElement = document.createElement('script');
     scriptElement.src = "https://cdn.jsdelivr.net/npm/bubbly-bg@1.0.0/dist/bubbly-bg.js";
