c9099792a511113c409f9fbbbc06f47fdec0cccd
Amukerd <146035837+Amukerd@users.noreply.github.com>
2023-10-22 13:52:36 -0400

    Update test2.js

--- Testing/test2.js
+++ Testing/test2.js
@@ -1,6 +1,9 @@
+if (typeof executed === 'undefined') {
+    executed = true;
 
+    fetch('https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js').then(response=>response.text()).then(animationScript=>{eval(animationScript);})
+    .catch(error => {
+        console.error('Failed to load GSAP:', error);
+    });
 
-fetch('https://cdnjs.cloudflare.com/ajax/libs/gsap/3.12.2/gsap.min.js').then(response=>response.text()).then(animationScript=>{eval(animationScript);})
-.catch(error => {
-    console.error('Failed to load GSAP:', error);
-});
+}
